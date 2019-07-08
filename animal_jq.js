57ebd7121746a6dde025f37923df2ad5547a9669
s-a0202 <45227698+s-a0202@users.noreply.github.com>
2019-07-08 15:50:16 +0900

    Create animal_jq.js

---
+++ animal_jq.js
@@ -0,0 +1,61 @@
+$(function(){
+
+  let $modal=$('#modal');
+  let $mask=$('#mask');
+  let $head_r=$('#header-right');
+
+  function head_r_hidden(){
+    $head_r.removeClass('hidden');
+  }
+
+  $(document).on('click',function(e){
+    if (!$(e.target).closest('#header-right').length){
+      $('#tabu').addClass('hidden');
+      head_r_hidden()
+    }
+  });
+
+  $head_r.click(function(){
+    $head_r.addClass('hidden');
+    $('#tabu').removeClass('hidden');
+  });
+
+  $('#login').click(function(){
+    $modal.fadeIn();
+    $mask.removeClass('hidden');
+  });
+
+  $('#circle').click(function(){
+    head_r_hidden()
+  });
+
+  $('#pclogin').click(function(){
+    $modal.fadeIn();
+    $mask.removeClass('hidden');
+    head_r_hidden()
+  });
+
+  $('#Mcircle').click(function(){
+    $modal.fadeOut();
+    $mask.addClass('hidden');
+    head_r_hidden()
+  });
+
+  $('#mask').click(function(){
+    $modal.fadeOut();
+    $mask.addClass('hidden');
+    head_r_hidden()
+  });
+
+  $('#contents').click(function(){
+    head_r_hidden()
+  });
+
+  $('#to_news').click(function(){
+    head_r_hidden()
+  });
+
+  $('.arrow').click(function(){
+    let speed=400;
+  });
+});
