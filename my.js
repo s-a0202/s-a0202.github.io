0d164b33a51eff63a23bef2337ff2d91c37b4f77
s-a0202 <45227698+s-a0202@users.noreply.github.com>
2020-03-23 18:38:14 +0900

    Update my.js

--- my.js
+++ my.js
@@ -1,24 +1,24 @@
 'use strict'
 {
-let idconfirm=document.getElementById('idconfirm');
-let pasconfirm=document.getElementById('pasconfirm');
-let limit=10;
+  let idconfirm=document.getElementById('idconfirm');
+  let pasconfirm=document.getElementById('pasconfirm');
+  let limit=10;
 
-document.getElementById('input1').addEventListener('click',()=>{
-  input1.classList.add('reClolor');
-});
+  document.getElementById('input1').addEventListener('click',()=>{
+    input1.classList.add('reClolor');
+  });
 
-document.getElementById('input2').addEventListener('keyup',()=>{
-  let input2len=input2.value.length;
-  if (input2len<6) {
-    pasconfirm.classList.add('chcolor');
-  }else if (input2len==0){
-    pasconfirm.classList.remove('chcolor');
-  }else if (input2len>7&&input2len<12) {
-    pasconfirm.classList.remove('chcolor');
-  }else if (input2len>12) {
-    pasconfirm.classList.add('chcolor');
-    alert('文字数超過です。12文字以内で入力してください')
-  }
-});
+  document.getElementById('input2').addEventListener('keyup',()=>{
+    let input2len=input2.value.length;
+    if (input2len<6) {
+      pasconfirm.classList.add('chcolor');
+    }else if (input2len==0){
+      pasconfirm.classList.remove('chcolor');
+    }else if (input2len>7&&input2len<12) {
+      pasconfirm.classList.remove('chcolor');
+    }else if (input2len>12) {
+      pasconfirm.classList.add('chcolor');
+      alert('文字数超過です。12文字以内で入力してください')
+    }
+  });
 }
